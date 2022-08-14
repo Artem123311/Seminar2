284cdead904d35b71d0c33db8fc297cb1a9454bf
Artem <mail@example.com>
2022-08-14 23:54:32 +0300

    Показать кубы чисел, заканчивающихся на четную цифру

---
+++ Project02/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] numbers = new int[]{1,2,3,4,5,6};
+ 
+for (int x = 0; x < numbers.Length; x++)
+{
+     int number = numbers[x];
+     int cube = (int)Math.Pow(number, 3);
+     if (cube % 2 == 0)
+          System.Console.WriteLine("Куб числа " + number + " равен " + cube + " и заканчивается на чётную цифру");
+}
