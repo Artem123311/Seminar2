004c44d9569ef4fad6eee1a9abca429b769954d5
Artem <mail@example.com>
2022-08-15 00:08:24 +0300

    Определить количество цифр в числе

---
+++ Project03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+public static int Main()
+    {
+        Console.Write("Введите число : ");
+        int num = int.Parse(Console.ReadLine ());
+        int i = 0;
+        while(num > 0)
+        {
+        i++;
+        num/= 10;
+        }
+    Console.WriteLine("Количество цифр введенного числа : {0}", i);
+    Console.ReadKey();
+    return 0;
+    }
+}
