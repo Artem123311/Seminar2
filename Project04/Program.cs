544bdbc0834c22b8c5e14cd5ec9f531870583afd
Artem <mail@example.com>
2022-08-15 00:20:51 +0300

    Найти сумму чисел от 1 до А

---
+++ Project04/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+ 
+class Summa {
+static void Main() {
+int n = Convert.ToInt32(Console.ReadLine());
+int i;
+int sum = 0;
+for (i=1; i <=n; i++) {
+sum = sum+i;
+}
+Console.WriteLine(sum);
+}
+}
